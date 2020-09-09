2b0683fc36f1f17d99c74bb96704bc81dd68bb58
NikitaPimenov <71039786+NikitaPimenov@users.noreply.github.com>
2020-09-09 22:51:44 +0300

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-systemctl stop dorm
-poetry install
-poetry run dorm_server/manage.py migrate
-poetry run dorm_server/manage.py collectstatic -c --noinput
-poetry run dorm_server/manage.py check --deploy
-systemctl start dorm
